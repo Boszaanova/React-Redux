1adf67652b08431840ad70ee2aa0dda7e7394940
boszaanova <boszaanova@gmail.com>
2022-11-06 19:00:58 +0700

    h

--- react-web-development/client/src/App.js
+++ react-web-development/client/src/App.js
@@ -4,7 +4,9 @@ import Home from "./containers/Home";
 import About from "./containers/About";
 import Order from "./containers/order/Order";
 import Product from "./containers/product/Product";
+import ProductEdit from "./containers/product/ProductEdit";
 import NotFound from "./containers/error/NotFound";
+
 
 
 
@@ -18,7 +20,10 @@ class App extends Component {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
         <Route exact path="/orders" element={<Order />} />
+
         <Route exact path="/products" element={<Product />} />
+        <Route exact path="/products/add" element={<ProductEdit />} />
+        <Route exact path="/products/edit/:id" element={<ProductEdit />} />
 
         <Route component={NotFound} />
       </Routes> 
---
+++ react-web-development/client/src/containers/product/ProductEdit.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Header from "../../components/Header";
+import ProductForm from "../../components/product/ProductForm";
+import Footer from "../../components/Footer";
+
+
+class ProductEdit extends Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <div className="container col-md-5">
+                   
+                   <h1>5555</h1>
+                    {/* <ProductForm /> */}
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+
+}
+
+export default ProductEdit;
