4f7d850c4eee0682c9ea9c9f8ee1cb25d0dec418
jakraphan <93023564+Boszaanova@users.noreply.github.com>
2022-11-09 11:58:59 +0700

    x

--- react-web-development/client/src/components/product/ProductForm.js
+++ react-web-development/client/src/components/product/ProductForm.js
@@ -35,66 +35,59 @@
 // export default ProductForm;
 
 
-import React, { Component } from "react";
+import React from "react";
 
 class ProductForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {value: ''};
-  
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-  
-    handleChange(event) {
-      this.setState({value: event.target.value});
-    }
-  
-    handleSubmit(event) {
-      alert('A name was submitted: ' + this.state.value);
-      event.preventDefault();
-    }
-  
-    render() {
-      return (
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
 
-        <div className="container col-md-6 p-5">
-        <form className="col-md-4" onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-
-        <select>
-  <option value="grapefruit">Grapefruit</option>
-  <option value="lime">Lime</option>
-  <option selected value="coconut">Coconut</option>
-  <option value="mango">Mango</option>
-</select>
-
-<input type="file" />
-
-        <textarea>
-  Hello there, this is some text in a text area
-</textarea>
-
-        <input className="btn btn-primary" type="submit" value="Submit" />
-          
-        </form>
-        </div>
-      );
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      
+
+      <div className="container col-md-5 my-5">
+        <form onSubmit={this.handleSubmit}>
+
+          <h2 className="my-5 text-primary text-center">Add Product</h2>
+
+          <div class="form-group my-2">
+            <label>Product Name:</label>
+            <input className="form-control" type="text" value={this.state.value} onChange={this.handleChange} />
+          </div>
+
+          <div class="form-group my-2">
+            <label>Unit Price:</label>
+            <input className="form-control" type="text" value={this.state.value} onChange={this.handleChange} />
+          </div>
+
+          <div class="form-group my-3">
+            <label>Upload Picture:</label>
+            <input className="form-control" type="file" />
+      
+          </div>
+       
+
+
+          <input className="btn btn-primary btn-lg" type="submit" value="Submit" />
+
+        </form>
+      </div>
+    );
+  }
+}
+
 export default ProductForm;
-  
